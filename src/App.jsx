30601d62fe3c2062356f4b9a57e828b3bf598c3f
espindolasol95 <espindolasol.ok@gmail.com>
2025-08-31 11:38:14 +0200

    feat(creazione del array di oggeti (initial articles) )

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+//array iniziale 
+ const initialArticles = [ 
+  {
+  id:1,
+  title:  "Monkey D. Luffy"
+  },
+  {
+  id:2,
+  title: "Roronoa Zoro"
+  },
+  {
+  id:3,
+  title: "Tony Tony Chopper"
+  },
+  {
+  id:4,
+  title: "Franky"
+  },
+  {
+  id:5,
+  title: "Nico Robin"
+  },
+];
+
+function App() {
+  const [articls, setArticles] = useState([
+   
+  ])
+
+}
+
+export default App
