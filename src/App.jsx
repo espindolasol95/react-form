a802e74facdfd6ec3c0fb54510cdbd9c74ad327d
espindolasol95 <espindolasol.ok@gmail.com>
2025-08-31 23:25:13 +0200

    feat (creazione usando boostrap del tittolo card, form e button)

--- src/App.jsx
+++ src/App.jsx
@@ -26,23 +26,52 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   //stati
-  const [articls, setArticles] = useState
+  const [articles, setArticles] = useState(initialArticles);
   const [newTitle, setNewTitle] = useState("");
- //aggiunta card
-  const handleSubmint = (event) =>{
+  const [nextId, setNextId] = useState(initialArticles.length + 1);
+ //funzione per aggiunta della card
+  const handleSubmit = (event) =>{
     event.preventDefault();
    return newTitle.trim()
    ? (setArticles([...articles, { id: nextId, title: newTitle }]),
         setNextId(nextId + 1),
-        setNewTitle(""))
-      : null;
-   
-     
+        setNewTitle("")
+      )
+      : null
+  };    
 
-  }
+  //funzione per cancellare la card
+   const handleDelete = (id) => {
+    setArticles(articles.filter((article) => article.id !== id));
+  };
+  
+ return(
+  <div className='container'>
+    <h1 className='text-center mb-5'>One Piece Card</h1>
+    <div className='row mb-4'>
+      <div className='col-md-6 offset-md-3'>
+        <form onSubmit={handleSubmit} className="d-flex">
 
-  
-  
+          <input 
+           type="text" 
+           name="title"
+           className="form-control me-2"
+           placeholder="Aggiungi nuova carta"
+          />
+        
+         <button type="submit" className="btn btn-primary">
+          Aggiungi
+          </button>  
+        </form>
+       
+        
+
+      </div>
+    </div>
+  </div>
+
+ )
+
 
 }
 
